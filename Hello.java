6014e8804e5ea4f2b5bf11accb6e9ca76fdc27ee
loganberry9298 <lcoverdale@plu.edu>
2021-03-10 11:54:00 -0800

    Step 4

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello");
-    System.out.println("Step 3b");
+    System.out.println("Step 4");
     System.out.println("beep boop");
   }
 }
