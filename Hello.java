238e83f842d760466cc9114f3af9929af78e2766
loganberry9298 <lcoverdale@plu.edu>
2021-03-10 11:42:50 -0800

    Step 3b

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello");
-    System.out.println("Step 2");
+    System.out.println("Step 3b");
     System.out.println("beep boop");
   }
 }
